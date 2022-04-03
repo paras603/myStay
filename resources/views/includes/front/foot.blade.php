85de54b750a3c2cabbd0494ba708335276cfc43e
paras603 <parasmanirai1@gmail.com>
2022-04-03 22:48:28 +0545

    file refactor

--- resources/views/includes/front/foot.blade.php
+++ resources/views/includes/front/foot.blade.php
@@ -1,2 +1,4 @@
+<script src="{{ asset('assets/front/js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{asset('assets/front/plugins/bootstrap-5.0.2-dist/js/bootstrap.min.js')}}"></script>
-
+<script src="{{ asset('assets/front/plugins/OwlCarousel2-2.3.4/dist/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/front/js/custom-front.js') }}"></script>
--- resources/views/includes/front/head.blade.php
+++ resources/views/includes/front/head.blade.php
@@ -61,32 +61,5 @@
 {{-- bootstrap icon --}}
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script type="text/javascript">
-        $('.owl-carousel').owlCarousel({
-            loop:true,
-            margin:10,
-            nav:false,
-            autoplay:false,
-            autoplayTimeout: 1500,
-            dots:false,
-            nav:false,
-            // navText:["<",">"],
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:3
-                },
-                950:{
-                    items:4
-                },
-                1150:{
-                    items:5
-                }
-            }
-        })
-
-    </script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+  
