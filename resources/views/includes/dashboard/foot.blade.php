4a98f09ac777ccc4ea513264495622a5ce6f5d8a
paras603 <parasmanirai1@gmail.com>
2022-04-04 00:17:24 +0545

    dashboard integrated

---
+++ resources/views/includes/dashboard/foot.blade.php
@@ -0,0 +1,23 @@
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src={{ asset('assets/dashboard/js/jquery.min.js') }}></script>
+<!-- Bootstrap -->
+<script src={{ asset('assets/dashboard/js/bootstrap/bootstrap.bundle.min.js') }}></script>
+<!-- overlayScrollbars -->
+<script src={{ asset('assets/dashboard/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}></script>
+<!-- AdminLTE App -->
+<script src={{ asset('assets/dashboard/js/adminlte.js') }}></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src={{ asset('assets/dashboard/plugins/jquery-mousewheel/jquery.mousewheel.js') }}></script>
+<script src={{ asset('assets/dashboard/plugins/raphael/raphael.min.js') }}></script>
+<script src={{ asset('assets/dashboard/plugins/jquery-mapael/jquery.mapael.min.js') }}></script>
+<script src={{ asset('assets/dashboard/plugins/jquery-mapael/maps/usa_states.min.js') }}></script>
+<!-- ChartJS -->
+<script src={{ asset('assets/dashboard/plugins/chart.js/Chart.min.js') }}></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src={{ asset('assets/dashboard/js/demo.js') }}></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src={{ asset('assets/dashboard/js/dashboard2.js') }}></script>
