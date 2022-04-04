120334dd86ae1ae459a4554332b8fc36bbc5ab24
paras603 <parasmanirai1@gmail.com>
2022-04-04 20:02:51 +0545

    merchant table updated

--- resources/views/front/merchant.blade.php
+++ resources/views/front/merchant.blade.php
@@ -6,17 +6,140 @@
             @csrf
             <div class="row">
                 <div class="col-lg-12 col-sm-12 account-settings-head">
-                    <h3>Account Settings</h3>
+                    <h3>Verify Details</h3>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-12 col-sm-12 account-settings-title">
-                    <h6>Verify Details</h6>
+                    <h6>Merchant Details</h6>
+                </div>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12">
+                    <h6><i class="bi bi-image-fill"></i>PP Size Image</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control @error('pp_image') is-invalid @enderror h-min" aria-label="" aria-describedby="basic-addon1"
+                               name="pp_image" onchange="loadPreview(this, '#pp_image')">
+                        @error('pp_image')
+                        <div class="invalid-feedback" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                        <div class="hs_preview_image_container d-none">
+                            <img id="pp_image" src="" class="img-fluid "  alt=""/>
+                            <a href="!#" class="hs_preview_image_close"><i class="fas fa-times"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <p><b>Citizenship card</b></p>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12">
+                    <h6><i class="bi bi-person"></i>Identity Front</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control @error('identity_front') is-invalid @enderror h-min" aria-label="" aria-describedby="basic-addon1"
+                               name="identity_front" onchange="loadPreview(this, '#identity_front')">
+                        @error('identity_front')
+                        <div class="invalid-feedback" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                        <div class="hs_preview_image_container d-none">
+                            <img id="identity_front" src="" class="img-fluid "  alt=""/>
+                            <a href="!#" class="hs_preview_image_close"><i class="fas fa-times"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12">
+                    <h6><i class="bi bi-person-fill"></i>Identity Back</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control @error('identity_back') is-invalid @enderror h-min"
+                               placeholder="Address" aria-label=""
+                               aria-describedby="basic-addon1" name="identity_back"  onchange="loadPreview(this, '#identity_back')">
+                        @error('identity_back')
+                        <div class="invalid-feedback" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                        <div class="hs_preview_image_container d-none">
+                            <img id="identity_back" src="" class="img-fluid "  alt=""/>
+                            <a href="!#" class="hs_preview_image_close"><i class="fas fa-times"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 account-settings-title">
+                    <h6>Homestay Details</h6>
                 </div>
             </div>
             <div class="row account-settings-content align-items-center">
                 <div class="col-lg-2 col-md-3 col-sm-12 ">
-                    <h6><i class="bi bi-person-circle"></i>Pan number</h6>
+                    <h6><i class="bi bi-house-door-fill"></i>Homestay Name</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control @error('homestay_name') is-invalid @enderror" placeholder="Homestay Name" aria-label="homestay name" aria-describedby="basic-addon1"
+                                   name="homestay_name" value="{{old('homestay_name')}}">
+                            @error('homestay_name')
+                            <div class="invalid-feedback" role="alert">
+                                {{$message}}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12 ">
+                    <h6><i class="bi bi-geo-alt-fill"></i>Address</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control @error('homestay_address') is-invalid @enderror" placeholder="Address" aria-label="homestay address" aria-describedby="basic-addon1"
+                                   name="homestay_address" value="{{old('homestay_address')}}">
+                            @error('homestay_address')
+                            <div class="invalid-feedback" role="alert">
+                                {{$message}}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12 ">
+                    <h6><i class="bi bi-telephone-fill"></i>Contact</h6>
+                </div>
+                <div class="col-lg-8 col-md-7 col-sm-12">
+                    <div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control @error('homestay_telephone') is-invalid @enderror" placeholder="Telephone Number" aria-label="Telephone Number" aria-describedby="basic-addon1"
+                                   name="homestay_telephone" value="{{old('homestay_telephone')}}">
+                            @error('homestay_telephone')
+                            <div class="invalid-feedback" role="alert">
+                                {{$message}}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row account-settings-content align-items-center">
+                <div class="col-lg-2 col-md-3 col-sm-12 ">
+                    <h6><i class="bi bi-card-text"></i>Pan number</h6>
                 </div>
                 <div class="col-lg-8 col-md-7 col-sm-12">
                     <div>
@@ -34,44 +157,26 @@
             </div>
             <div class="row account-settings-content align-items-center">
                 <div class="col-lg-2 col-md-3 col-sm-12">
-                    <h6><i class="bi bi-person"></i>Identity Front</h6>
+                    <h6><i class="bi bi-file-earmark-pdf-fill"></i>Registration Certificate</h6>
                 </div>
                 <div class="col-lg-8 col-md-7 col-sm-12">
                     <div class="input-group mb-3">
-                        <input type="file" class="form-control @error('identity_front') is-invalid @enderror h-min" aria-label="Username" aria-describedby="basic-addon1"
-                               name="identity_front" onchange="loadPreview(this, '#identity_front')">
-                        @error('identity_front')
-                        <div class="invalid-feedback" role="alert">
+                        <input type="file" class="form-control @error('registration_certificate') is-invalid @enderror h-min"
+                               placeholder="" aria-label="registration_certificate"
+                               aria-describedby="basic-addon1" name="registration_certificate"  onchange="loadPreview(this, '#registration_certificate')">
+                        @error('identity_back')
+                        <div class="registration_certificate" role="alert">
                             {{$message}}
                         </div>
                         @enderror
                         <div class="hs_preview_image_container d-none">
-                            <img id="identity_front" src="" class="img-fluid "  alt=""/>
+                            <img id="registration_certificate" src="" class="img-fluid "  alt=""/>
                             <a href="!#" class="hs_preview_image_close"><i class="fas fa-times"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row account-settings-content align-items-center">
-                <div class="col-lg-2 col-md-3 col-sm-12">
-                    <h6><i class="bi bi-geo-alt-fill"></i>Identity Back</h6>
-                </div>
-                <div class="col-lg-8 col-md-7 col-sm-12">
-                    <div class="input-group mb-3">
-                        <input type="file" class="form-control @error('identity_back') is-invalid @enderror h-min"
-                               placeholder="Address" aria-label="Username"
-                               aria-describedby="basic-addon1" name="identity_back"  onchange="loadPreview(this, '#identity_back')">
-                        @error('identity_back')
-                        <div class="invalid-feedback" role="alert">
-                            {{$message}}
-                        </div>
-                        @enderror
-                        <div class="hs_preview_image_container d-none">
-                            <img id="identity_back" src="" class="img-fluid "  alt=""/>
-                            <a href="!#" class="hs_preview_image_close"><i class="fas fa-times"></i></a>
-                        </div>
-                    </div>
-                </div>
+            <div style="padding:20px 0px;">
             </div>
             <div>
                 <button type="submit" class="btn btn-primary">Make me a merchant</button>
