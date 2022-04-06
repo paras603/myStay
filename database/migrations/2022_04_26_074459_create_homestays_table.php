9f3929a0f872eef474bcad1c64159f1b8578e172
paras603 <parasmanirai1@gmail.com>
2022-04-06 10:27:42 +0545

    ..

--- database/migrations/2022_04_26_074459_create_homestays_table.php
+++ database/migrations/2022_04_26_074459_create_homestays_table.php
@@ -15,14 +15,12 @@ class CreateHomestaysTable extends Migration
     {
         Schema::create('homestays', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD:database/migrations/2022_02_26_074459_create_homestays_table.php
-            $table->string('hs_name');
             
             $table->string('hs_imgs');
             $table->longText('hs_services');
             $table->longText('nearby_places');
             $table->string('iframe');
-=======
+            
             $table->string('pan_number');
             $table->string('homestay_name');
             $table->string('homestay_address');
@@ -30,7 +28,6 @@ class CreateHomestaysTable extends Migration
             $table->string('registration_certificate');
             $table->unsignedBigInteger('merchant_id')->index();
             $table->foreign('merchant_id')->references('id')->on('merchants')->onDelete('cascade');
->>>>>>> f518584ab003a8163264bb5f2d8323a1f7850b81:database/migrations/2022_04_26_074459_create_homestays_table.php
             $table->timestamps();
         });
     }
