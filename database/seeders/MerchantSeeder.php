dad098aaa47cabb2124ee9b300380582ea5d120d
Sanjeev Bhandari <sanjeevvsanjeev1@gmail.com>
2022-04-06 07:49:02 +0545

    merchantseeder

--- database/seeders/MerchantSeeder.php
+++ database/seeders/MerchantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Merchant;
 use Illuminate\Database\Seeder;
 
 class MerchantSeeder extends Seeder
@@ -13,6 +14,25 @@ class MerchantSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Merchant::upsert([
+            [
+                'id'                        =>  1,
+                'identity_front'            => 'sfsfsf.jpg',
+                'identity_back'             => 'sfsfsf.png',
+                'merchant_image'            =>  'sfsfsf.jpg',
+                'verified'                  => 'sanjeevvsanjeev1@gmail.com',
+                'user_id'                   =>  1,
+                'created_at'                => now(),
+            ],
+            [
+                'id'                        =>  1,
+                'identity_front'            => 'sfsfsf.jpg',
+                'identity_back'             => 'sfsfsf.png',
+                'merchant_image'            =>  'sfsfsf.jpg',
+                'verified'                  => 'sanjeevvsanjeev1@gmail.com',
+                'user_id'                   =>  2,
+                'created_at'                => now(),
+            ],
+        ],[''],[]);
     }
 }
