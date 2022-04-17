b499ba098f9cecbc60f3698f83ea275ad4c9efd5
paras603 <parasmanirai1@gmail.com>
2022-04-17 20:04:37 +0545

    bookmark on progress

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Front;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Blog;
 
-class BlogController extends Controller
+class BookmarkController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,19 +13,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $user = \auth()->user();
-        $blogs = Blog::where('blog_author',$user->id)->get();
-        return view('front.blog.index', compact('blogs'));
-    }
-
-    public function blogs(){
-        $blogs = Blog::all();
-        return view('front.blog.blogs', compact('blogs'));
-    }
-
-    public function blog($id){
-        $blog = Blog::where('id',$id)->first();
-        return view('front.blog.view', compact('blog'));
+        //
     }
 
     /**
@@ -37,7 +23,7 @@ class BlogController extends Controller
      */
     public function create()
     {
-        return view('front.blog.create');
+        //
     }
 
     /**
@@ -48,23 +34,7 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        $author = auth()->user();
-
-        $image = $request->file('blog_image');
-        $imageName = HomestayHelper::renameImageFileUpload($image);
-        $image->storeAs(
-            'public/uploads/blogs/',$imageName
-        );
-        
-        Blog::create([
-            'blog_title'            => $request->input('blog_title'),
-            'blog_detail'           => $request->input('blog_detail'),
-            'blog_image'            => $imageName,
-            'blog_author'           => $author,
-            'published_date'        => now(),
-        ]);
-
-        return redirect()->route('front.index')->with('toast.success', 'Room added');
+        //
     }
 
     /**
